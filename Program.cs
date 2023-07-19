52b985239225d34dbfad3ec63388e49eb57775ae
Shubham <shubhambawa98@gmail.com>
2023-07-19 11:01:27 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World! Somrthing");
 Console.WriteLine("The current time is NIe " + DateTime.Now);
-
-
